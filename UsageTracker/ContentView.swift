d32c1b85b54605e3a4995402b16398cc4aea1504
Ivan Peltzer <shpeltzer17@gmail.com>
2026-07-11 15:58:28 +0300

    Initial Commit

---
+++ UsageTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UsageTracker
+//
+//  Created by Ivan Peltzer on 11.07.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UsageTracker/UsageTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsageTrackerApp.swift
+//  UsageTracker
+//
+//  Created by Ivan Peltzer on 11.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct UsageTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
